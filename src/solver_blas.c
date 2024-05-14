4d9515e9453f6553e13f1428e827079feadb309c
monicaarghir4 <marghir4@gmail.com>
2024-05-14 21:55:11 +0300

    try-blas

--- src/solver_blas.c
+++ src/solver_blas.c
@@ -3,12 +3,44 @@
  * 2024 Spring
  */
 #include "utils.h"
+#include <cblas.h>
 
 /* 
  * Add your BLAS implementation here
  */
-//oki
+
 double* my_solver(int N, double *A, double *B) {
-	printf("BLAS SOLVER\n");
-	return NULL;
+	double* first_calc = (double *)malloc(N * N * sizeof(double));
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			first_calc[i * N + j] = B[i * N + j];
+		}
+	}
+
+	cblas_dtrmm(CblasRowMajor, CblasLeft, CblasUpper, CblasTrans, CblasNonUnit,
+                N, N, 1.0, A, N, first_calc, N);
+
+	double* second_calc = (double *)malloc(N * N * sizeof(double));
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			second_calc[i * N + j] = B[i * N + j];
+		}
+	}
+
+	cblas_dtrmm(CblasRowMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit,
+				N, N, 1.0, A, N, second_calc, N);
+
+	cblas_daxpy(N * N, 1.0, first_calc, 1, second_calc, 1);
+
+	double* C = (double *)malloc(N * N * sizeof(double));
+
+
+	cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, N, N, N, 1.0,
+					second_calc, N, B, N, 0.0, C, N);
+
+	free(first_calc);
+	free(second_calc);
+	return C;
 }
